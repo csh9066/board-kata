d58b0c99beda0e4f916362cf2dbdcf4b7163a9fe
seunglo <devseung9056@gmail.com>
2022-07-05 02:34:58 +0900

    AppConfig에 있는 PasswordEncoder WebSecurityConfig로 이전

--- sever/src/main/java/boardkata/sever/securituy/WebSecurityConfig.java
+++ sever/src/main/java/boardkata/sever/securituy/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package boardkata.sever.securituy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,5 +16,10 @@ public class WebSecurityConfig {
                 .csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
