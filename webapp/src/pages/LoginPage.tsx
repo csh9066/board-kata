eee051d5a4e171bdc2297800c6386fd24d16a4c2
seunglo <devseung9056@gmail.com>
2022-07-05 02:05:07 +0900

    로그인 페이지 구현

---
+++ webapp/src/pages/LoginPage.tsx
@@ -0,0 +1,61 @@
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import AuthTemplate from "../components/AuthTemplate";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const onLogin = async (form: LoginForm) => {
+    try {
+      await axios.post("http://localhost:8080/login", form);
+      navigate("/");
+    } catch (e: any) {
+      if (e?.response?.data.message) {
+        message.error(e?.response?.data.message);
+        return;
+      }
+
+      console.error(e);
+    }
+  };
+
+  return (
+    <AuthTemplate
+      title="로그인"
+      footer={<Link to={"/sign-up"}>회원 가입</Link>}
+    >
+      <Form onFinish={onLogin}>
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "이메일을 입력해주세요" }]}
+        >
+          <Input size="large" type="email" placeholder="이메일" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[
+            { required: true, message: "패스워드를 입력해주세요" },
+            {
+              min: 8,
+              max: 16,
+              message: "비밀번호는 8 ~ 16 값입니다.",
+            },
+          ]}
+        >
+          <Input size="large" type="password" placeholder="패스워드"></Input>
+        </Form.Item>
+
+        <Button block type="primary" size="large" htmlType="submit">
+          로그인
+        </Button>
+      </Form>
+    </AuthTemplate>
+  );
+}
