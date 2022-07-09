aad1df86154f0b830533166e5dd43e67bc4539ad
seunglo <devseung9056@gmail.com>
2022-07-10 04:56:10 +0900

    IndexPage에서 헤더 컴포넌트 분리

---
+++ webapp/src/components/Header.tsx
@@ -0,0 +1,65 @@
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  Layout,
+  Menu,
+  Space,
+  Typography,
+} from "antd";
+import { CaretDownOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+function menu(logout: any) {
+  return (
+    <Menu
+      items={[
+        {
+          key: 1,
+          label: "로그 아웃",
+          onClick: logout,
+        },
+      ]}
+    />
+  );
+}
+
+function Header() {
+  const { isLoggedIn, logout, me } = useAuth();
+
+  return (
+    <Layout.Header
+      style={{
+        background: "transparent",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/">
+        <Typography.Text strong>BOARD KATA</Typography.Text>
+      </Link>
+      <div>
+        {isLoggedIn ? (
+          <Space>
+            <Button type="default" shape="round">
+              새 글 작성
+            </Button>
+            <Dropdown overlay={menu(logout)}>
+              <Space>
+                <Avatar>{me?.nickname[0]}</Avatar>
+                <CaretDownOutlined />
+              </Space>
+            </Dropdown>
+          </Space>
+        ) : (
+          <Button type="primary">
+            <Link to="/login">로그인</Link>
+          </Button>
+        )}
+      </div>
+    </Layout.Header>
+  );
+}
+
+export default Header;
--- webapp/src/pages/IndexPage.tsx
+++ webapp/src/pages/IndexPage.tsx
@@ -1,26 +1,10 @@
-import { Button, Layout, Typography } from "antd";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Layout } from "antd";
+import Header from "../components/Header";
 
 export default function IndexPage() {
   return (
     <Layout>
-      <Layout.Header
-        style={{
-          background: "transparent",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Link to="/">
-          <Typography.Text strong>BOARD KATA</Typography.Text>
-        </Link>
-        <div>
-          <Button type="primary">
-            <Link to="/login">로그인</Link>
-          </Button>
-        </div>
-      </Layout.Header>
+      <Header />
       <Layout.Content></Layout.Content>
     </Layout>
   );
