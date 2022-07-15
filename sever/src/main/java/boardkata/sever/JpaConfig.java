45b58fa106d31c9c5fba1d29a3f45da223072f93
seunglo <devseung9056@gmail.com>
2022-07-15 21:00:24 +0900

    Querydsl에 관한 build.gradle 및 설정 추가

--- sever/src/main/java/boardkata/sever/JpaConfig.java
+++ sever/src/main/java/boardkata/sever/JpaConfig.java
@@ -1,9 +1,22 @@
 package boardkata.sever;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
